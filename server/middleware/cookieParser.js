fbf920d95e65983c74a7316e8d3c295c2f860e06
Mantas Kukulskis <8mantas8@gmail.com>
2025-07-01 21:27:15 +0300

    Admin movies

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,16 +1,18 @@
 export function cookieParser(req, res, next) {
-  req.cookies = {};
+    req.cookies = {};
 
-  const cookieStr = req.headers.cookie;
-  if (!cookieStr) {
+    const cookieStr = req.headers.cookie;
+    if (!cookieStr) {
+        return next();
+    }
+
+    const cookieParts = cookieStr
+        .split(';')
+        .map(s => s.trim().split('='));
+
+    for (const [key, value] of cookieParts) {
+        req.cookies[key] = value;
+    }
+
     return next();
-  }
-
-  const cookieParts = cookieStr.split(";").map((s) => s.trim().split("="));
-
-  for (const [key, value] of cookieParts) {
-    req.cookies[key] = value;
-  }
-
-  return next();
 }
